24c9e97675c6781cd638955b0b615a5629883de3
David Pedersen <david.pdrsn@gmail.com>
2013-10-10 15:08:03 +0200

    fix things after moving things around

--- demo_server.rb
+++ demo_server.rb
@@ -3,6 +3,17 @@
 require 'sinatra'
 require 'json'
 
+get '/' do
+  send_file 'demo.html'
+end
+
+get '/jquery.sayt.js' do
+  send_file 'jquery.sayt.js'
+end
+
+get '/demo.js' do
+  send_file 'demo.js'
+end
 
 get '/ajax' do
   response = []
